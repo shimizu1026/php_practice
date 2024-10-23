451ad0bd362c5ae74de5b13cd7a3b96fdf1e32cf
shimizu1026 <115125150+shimizu1026@users.noreply.github.com>
2024-10-23 11:03:02 +0900

    4-7

--- php/chapter4/checks-output.php
+++ php/chapter4/checks-output.php
@@ -1,8 +1,12 @@
-<?php require '../header.php'; ?>
-<?php
+<?php require '../header.php' ?>
+<?php 
+if (isset($_REQUEST['genre'])){
 foreach ($_REQUEST['genre'] as $item) {
-	echo '<p>', $item, '</p>';
+    echo '<p>', $item, '</p>';
+
 }
-echo 'に関するお買い得情報をお送りさせて頂きます。';
-?>
-<?php require '../footer.php'; ?>
+echo 'に関するお買い得情報をお送りさせていただきます。';
+}
+;?>
+
+<?php require '../footer.php' ?>
