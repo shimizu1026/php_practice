6465315ba8813963aff4114ec730baa5c8826729
shimizu1026 <115125150+shimizu1026@users.noreply.github.com>
2024-10-26 22:33:28 +0900

    4-7

--- php/chapter4/checks-input.php
+++ php/chapter4/checks-input.php
@@ -1,16 +1,12 @@
-<?php require '../header.php' ?>
-<p>ご興味のある商品ジャンルを選んでください。</p>
+<?php require '../header.php'; ?>
+<p>ご興味のある商品ジャンルを全て選んでください。</p>
 <form action="checks-output.php" method="post">
+<?php
+$genre=['カメラ','パソコン','時計','家電','書籍','文房具','食品'];?>
 <?php 
-    $genre=['カメラ', 'パソコン','時計','家電','書籍','文房具','食品'];
-    foreach ($genre as $item) {
-        echo '<p>';
-        echo '<input type="checkbox" name="genre[]" value="', $item, '">';
-        echo $item;
-        echo '</p>';
-    }
-
-;?>
-    <p><input type="submit" value="確定"></p>
+foreach($genre as $item):?>
+<p><input type="text" value="<?php echo '$item' ?>"></p>
+<?php endforeach; ?>
+<p><input type="submit" value="確定"></p>
 </form>
-<?php require '../footer.php' ?>
+<?php require '../footer.php'; ?>
