cff01914a07f3e757745f1497a8b27c26e3001cb
shimizu1026 <115125150+shimizu1026@users.noreply.github.com>
2024-11-21 18:45:31 +0900

    6

--- php/chapter6/search-input.php
+++ php/chapter6/search-input.php
@@ -1,6 +1,6 @@
 <?php require '../header.php'; ?>
 商品名を入力してください。
-<form action="search-output.php" method="post">
+<form action="search-output2.php" method="post">
 <input type="text" name="keyword">
 <input type="submit" value="検索">
 </form>
